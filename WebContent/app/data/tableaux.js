3e8cfb3ed9201bc6f0768da635a004ecabbdde3f
Steven Nesztler <steven.nesztler.ext@orange.com>
2018-11-08 12:48:43 +0100

    Modification des scenes

--- WebContent/app/data/tableaux.js
+++ WebContent/app/data/tableaux.js
@@ -1,48 +1,32 @@
 'use strict';
 define(["jquery"], function($){
-	var data = [
-		{
-			texte : {
-				position : "bas",
-				texte : "Premiere scene - Ciel etoilé"
-			},
-			image : "app/img/game/tableaux/1.png",
-			transitions : {
-				start : null,
-				end : null
-			}
-		},
-		{
-			texte : {
-				position : "haut",
-				texte : "Deuxieme scene - Tasse"
-			},
-			image : "app/img/game/tableaux/2.png",
-			transitions : {
-				start : null,
-				end : null
-			}
-		},
-		{
-			texte : {
-				position : "bas",
-				texte : "Troisieme scene - Chambre"
-			},
-			image : "app/img/game/tableaux/3.png",
-			transitions : {
-				start : null,
-				end : null
-			}
-		}
-	];
-	
-	return {
-		get : function(key) {
-			if (data[key]) return $.extend(true, {}, data[key]);
-			else return null;
-		},
-		list : function() {
-			return data;
-		}
-	};
+    var data = [];
+
+    addScene("Test1", "bas", "app/img/game/tableaux/1.png");
+    addScene("Test2", "haut", "app/img/game/tableaux/2.png");
+    addScene("Test3", "bas", "app/img/game/tableaux/3.png");
+
+    function addScene(texte, position, image) {
+        data.push({
+            texte : {
+                position : position,
+                texte : texte
+            },
+            image : image,
+            transitions : {
+                start : null,
+                end : null
+            }
+        });
+    };
+
+    return {
+        get : function(key) {
+            if (data[key]) return $.extend(true, {}, data[key]);
+            else return null;
+        },
+        list : function() {
+            return data;
+        }
+    };
 });
